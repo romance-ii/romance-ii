a5a62bd33d78261b939f11157de4a0e6efe016e1
Bruce-Robert Pocock <brpocock@Star-Hope.org>
2012-01-30 17:30:43 -0500

    the great Res code drop
    Former-commit-id: 04651e890254ff6186c18c4b51099a980cc92518

--- src/org/starhope/appius/except/ParameterException.java
+++ src/org/starhope/appius/except/ParameterException.java
@@ -5,30 +5,29 @@ package org.starhope.appius.except;
 
 /**
  * @author ewinkelman
- *
  */
 public class ParameterException extends Exception {
-
+	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 6973758282644073298L;
-
+	
 	/**
 	 * WRITEME
 	 */
 	private final String reason;
-
+	
 	/**
 	 * WRITEME: Document this constructor brpocock@star-hope.org
 	 * 
 	 * @param e WRITEME
 	 */
-	public ParameterException(final Exception e) {
+	public ParameterException (final Exception e) {
 		initCause (e);
 		reason = e.getMessage ();
 	}
-
+	
 	/**
 	 * @param message WRITEME
 	 */
@@ -36,14 +35,13 @@ public class ParameterException extends Exception {
 		super (message);
 		reason = message;
 	}
-
+	
 	/**
 	 * @see java.lang.Throwable#getMessage()
 	 */
 	@Override
 	public String getMessage () {
 		final String superMessage = super.getMessage ();
-		return null == superMessage ? reason : superMessage + "\t"
-				+ reason;
+		return null == superMessage ? reason : superMessage + "\t" + reason;
 	}
 }
