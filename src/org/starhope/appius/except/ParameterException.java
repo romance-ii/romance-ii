3ed30c948e38f946c73b2f0388678a9f860539d8
Bruce-Robert Pocock <brpocock@Shaitan.Shearer.Star-Hope.org>
2012-01-30 13:02:19 -0500

    Copying in from Subversion Trunk (initial copy)
    Former-commit-id: 02a6dc02ba55d5b7b7635a407b36e9e77ae938ae

---
+++ src/org/starhope/appius/except/ParameterException.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package org.starhope.appius.except;
+
+/**
+ * @author ewinkelman
+ *
+ */
+public class ParameterException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6973758282644073298L;
+
+	/**
+	 * WRITEME
+	 */
+	private final String reason;
+
+	/**
+	 * WRITEME: Document this constructor brpocock@star-hope.org
+	 * 
+	 * @param e WRITEME
+	 */
+	public ParameterException(final Exception e) {
+		initCause (e);
+		reason = e.getMessage ();
+	}
+
+	/**
+	 * @param message WRITEME
+	 */
+	public ParameterException (final String message) {
+		super (message);
+		reason = message;
+	}
+
+	/**
+	 * @see java.lang.Throwable#getMessage()
+	 */
+	@Override
+	public String getMessage () {
+		final String superMessage = super.getMessage ();
+		return null == superMessage ? reason : superMessage + "\t"
+				+ reason;
+	}
+}
