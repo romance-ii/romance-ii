e0d95fd017eddda14429f86a88b26e92e85c6b95
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2013-08-30 16:34:31 -0400

    wip
    Former-commit-id: 09704d2e3e62a7f0bbae1c2d2d44190781cbb3d6

---
+++ src/ps/00-preamble.lisp
@@ -0,0 +1,27 @@
+;; -*- lisp -*-
+
+(defpackage "VIOLET-VOLTS"
+  (:use "PARENSCRIPT"))
+
+(handler-bind
+    ((name-conflict
+      (lambda (c)
+        (declare (ignorable c))
+        (when (find-restart 'sb-impl::take-new)
+          (invoke-restart 'sb-impl::take-new))
+        (format *error-output*
+                "~& No TAKE-NEW restart on NAME-CONFLICT; ~&~S 
+ Found: ~{~% • ~S ~}~%" c (compute-restarts)))))
+  (use-package :ps))
+
+(setf parenscript::*js-string-delimiter* #\'
+      parenscript::*js-target-version*   1.6)
+
+(defmacro by-id (id)
+  `((@ document get-element-by-id) ,id))
+
+(defmacro concat (&rest stuff)
+  (append (list 'concatenate 'string) stuff))
+
+
+
