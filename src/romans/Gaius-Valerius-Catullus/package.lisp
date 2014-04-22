1b71fe6343a1516781e5ee8a6095180257d4b9d3
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2014-04-22 15:43:05 -0400

    use ROMANCE; exports

--- src/romans/Gaius-Valerius-Catullus/package.lisp
+++ src/romans/Gaius-Valerius-Catullus/package.lisp
@@ -1,4 +1,4 @@
-(defpackage :catullus (:use :cl :alexandria :bordeaux-threads)
+(defpackage :catullus (:use :cl :alexandria :bordeaux-threads :romance)
             (:nicknames :gaius-valerius-catullus)
             (:documentation "Catullus handles the textual interface whereby
 human-provided strings are parsed and tokenized into propositions
@@ -7,4 +7,6 @@ AI characters into string form.
 
 This module also handles the ConceptNet5 database.
 
-Gaius Valerius Catullus was a noted poet/songwriter."))
+Gaius Valerius Catullus was a noted poet/songwriter.")
+            (:export #:converse-repl #:server-start #:test-parser))
+
