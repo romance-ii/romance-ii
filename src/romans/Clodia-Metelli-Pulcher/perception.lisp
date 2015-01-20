cc89ce4672fac9b5c351a0bc130c1e43c24eb8ac
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2015-01-20 04:18:26 -0500

    perception

--- src/romans/Clodia-Metelli-Pulcher/perception.lisp
+++ src/romans/Clodia-Metelli-Pulcher/perception.lisp
@@ -1,19 +1,39 @@
 (in-package :clodia)
 
-(defclass sound-event nil nil)
-(defclass speech-event (sound-event) nil)
-(defclass visual-event nil nil)
-(defclass physics-event nil nil)
+(defclass sound-asset ()
+  ((kind )))
 
-(defgeneric stimulate (mind stimulus))
+(defclass stimulus-event ()
+  ((origin :initarg :origin :reader stimulus-origin)))
 
-(defmethod stimulate (mind (stimulus sound-event)) :TODO)
+(defclass visual-event (stimulus-event) nil)
 
-(defmethod stimulate (mind (stimulus speech-event)) :TODO)
+(defclass haptic-event (stimulus-event)
+  ((force :initarg :volume :reader haptic-force)))
 
-(defmethod stimulate (mind (stimulus visual-event)) :TODO)
+(defclass sound-event (haptic-event)
+  ((asset :initarg :asset :reader sound-asset)))
 
-(defmethod stimulate (mind (stimulus physics-event)) :TODO)
+(defclass speech-event (sound-event)
+  ((language :initarg :lang :reader speech-language)
+   (voice :initarg :voice :reader speech-voice)
+   (timbre :initarg :timbre :reader speech-timbre)
+   (dialog :initarg :dialog :reader dialog)))
+
+(defun sound-volume (sound-event)
+  (check-type sound-event sound-event)
+  (haptic-force sound-event))
+
+;;(defgeneric effective-force-at-point (origin force point))
+
+(defclass haptic-event (stimulus-event) nil)
+
+(defgeneric stimulate (observer stimulus))
+
+(define-condition stimulus-not-acceptable (warning)
+  ((stimulus :initarg :stimulus :reader stimulus))
+  (:report (lambda (c s)
+             (format s "The stimulus signaled was not acceptable to any observer."))))
 
 
 (defun pay-attention-to (mind matter) :TODO)
@@ -41,94 +61,6 @@
 (defun evaluate-character-virtue (mind character) :TODO)
 
 ;;; TODO: conversational interactions with Catullus
-
-(defun perform (mind plan) :TODO)
-
-(defun judge-game-state (mind environment) :TODO)
-(defun begin-game (mind environment game) :TODO)
-
-(defun leave-game (mind environment) :TODO)
-
-(defgeneric get-game-actions (mind environment game))
-(defgeneric adjudicate-game-action (mind environment game action))
-(defgeneric choose-next-game-action (mind environment game))
-
-
-
-(defmethod get-game-actions (mind environment (game (eql :football)))
-  :TODO)
-(defmethod adjudicate-game-action
- (mind environment (game (eql :football)) action)
-  :TODO)
-(defmethod choose-next-game-action (mind environment (game (eql :football)))
-  :TODO)
-
-
-(defmethod get-game-actions (mind environment (game (eql :volleyball)))
-  :TODO)
-(defmethod adjudicate-game-action
-    (mind environment (game (eql :volleyball)) action)
-  :TODO)
-(defmethod choose-next-game-action (mind environment (game (eql :volleyball)))
-  :TODO)
-
-
-
-(defmethod get-game-actions (mind environment (game (eql :baseball)))
-  :TODO)
-(defmethod adjudicate-game-action
-    (mind environment (game (eql :baseball)) action)
-  :TODO)
-(defmethod choose-next-game-action (mind environment (game (eql :baseball)))
-  :TODO)
-
-
-
-(defmethod get-game-actions (mind environment (game (eql :basketball)))
-  :TODO)
-(defmethod adjudicate-game-action
-    (mind environment (game (eql :basketball)) action)
-  :TODO)
-(defmethod choose-next-game-action (mind environment (game (eql :basketball)))
-  :TODO)
-
-
-
-(defmethod get-game-actions (mind environment (game (eql :zap-rocks)))
-  :TODO)
-(defmethod adjudicate-game-action
-    (mind environment (game (eql :zap-rocks)) action)
-  :TODO)
-(defmethod choose-next-game-action (mind environment (game (eql :zap-rocks)))
-  :TODO)
-
-(defmethod get-game-actions (mind environment (game (eql :pilot-craft)))
-  :TODO)
-(defmethod adjudicate-game-action
-    (mind environment (game (eql :pilot-craft)) action)
-  :TODO)
-(defmethod choose-next-game-action (mind environment (game (eql :pilot-craft)))
-  :TODO)
-
-
-(defmethod get-game-actions (mind environment (game (eql :mission-control)))
-  :TODO)
-(defmethod adjudicate-game-action
-    (mind environment (game (eql :mission-control)) action)
-  :TODO)
-(defmethod choose-next-game-action
- (mind environment (game (eql :mission-control)))
-  :TODO)
-
-
-(defun inspired-to-create-a-maze ()
-  :TODO)
-
-(defun inspired-to-bury-treasure () :TODO)
-
-(defun construction-crew-behaviour () :TODO)
-
-(defun singing-behavious () :TODO)
 
 
 
