43d641d7acf19f874da286aaecc55ac6a244f9d7
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2015-01-20 04:24:01 -0500

    submodule

--- src/romans/Clodia-Metelli-Pulcher/self.lisp
+++ src/romans/Clodia-Metelli-Pulcher/self.lisp
@@ -1,6 +1,14 @@
 (in-package :clodia)
 
-(defclass mind () ())
+(defclass mind-part () ())
+(defclass consciousness (mind-part) ())
+(defclass unconsciousness (mind-part) ())
+
+(defclass mind ()
+  ((consciousness :reader conscious
+                  :initform (make-instance 'consciousness) :initarg :conscious)
+   (unconsciousness :reader unconscious
+                    :initform (make-instance 'unconsciousness) :initarg :unconscious)))
 
 (defmethod print-object ((mind mind) s)
   (format s "#<MIND: can't print myself yet, FIXME!>"))
