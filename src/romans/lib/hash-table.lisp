fcd2a36f390a454c42f4902eb61ce6980d1206b6
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2015-05-20 09:59:28 -0400

    improved hash-table macro

--- src/romans/lib/hash-table.lisp
+++ src/romans/lib/hash-table.lisp
@@ -1,12 +1,18 @@
 (in-package :romans)
 
-(defmacro |hash| ((&rest params) plist-or-alist)
-  `(let ((h (make-hash-table ,@params))) 
-     ,(if (and (consp (car plist-or-alist))
-               (atomp (cdr (car plist-or-alist))))
-          ;; let's assume it's an alist
-          (loop for (key . value) in plist-or-alist
-             collecting `(setf (gethash ,key h) ,value))
-          ;; plist
-          (loop for (key value) in plist-or-alist :by #'cddr
-             collecting `(setf (gethash ,key h) ,value)))))
+(defmacro |hash| ((&key (size nil size-?) &rest params) plist-or-alist)
+  (let ((alistp (and (consp (car plist-or-alist))
+                     (atomp (cdr (car plist-or-alist))))))
+    `(let ((h (make-hash-table 
+               ,@(if size-?
+                     params
+                     (cons params (list :size (if alistp 
+                                                  (length plist-or-alist)
+                                                  (/ (length plist-or-alist) 2)))))))) 
+       ,(if alistp
+            ;; let's assume it's an alist
+            (loop for (key . value) in plist-or-alist
+               collecting `(setf (gethash ,key h) ,value))
+            ;; plist
+            (loop for (key value) in plist-or-alist :by #'cddr
+               collecting `(setf (gethash ,key h) ,value))))))
