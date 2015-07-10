707da756f3d0572d2040dc204b737d8e7f835591
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2015-07-10 16:19:22 -0400

    split out into a separate file to break dependency cycle

--- src/romans/lib/repl-glue.lisp
+++ src/romans/lib/repl-glue.lisp
@@ -182,22 +182,6 @@ If you're totally lost here, try (HELP :START)
 
 
 
-(defun start-repl (&optional argv)
-  (unless (member "--silent" argv :test #'string-equal)
-    (format t "~&~|~%Romance Ⅱ
-Copyright © 2013-2015, Bruce-Robert Fenn Pocock.
-Evaluate (ROMANCE:COPYRIGHTS T) for details.
-Read-Eval-Print-Loop interactive session.
-For help, evaluate (ROMANCE:REPL-HELP) (ie type: (HELP) at the prompt.)~2%"))
-  (unless (user-ident)
-    (format t "~&You haven't identified yourself. Say Hello!
-... enter (HELLO \"your name here\") to identify yourself.")
-    (setf *user-ident* (format nil "REPL user ~A" (gensym "REPL"))))
-  (caesar:with-oversight (repl)
-    (let ((*package* (find-package :romance-user)))
-      (prepl:repl :nobanner t :inspect t :continuable t))))
-
-
 
 (in-package :romance-user)
 
